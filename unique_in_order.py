14ccc51112733b1a8337c97eeb6fbdf713f30101
Samuel Ujazda <samuelujazda@gmail.com>
2022-06-06 13:34:50 +0200

    small change

--- unique_in_order.py
+++ unique_in_order.py
@@ -1,9 +1,9 @@
 iterable = 'AAAABBBCCDAABBB'
 def unique_in_order(iterable):
     result = []
-    poprzednia = 0
+    previous = 0
     for i in iterable:
-        if i != poprzednia:
+        if i != previous:
             result.append(i)
-        poprzednia = i
+        previous = i
     return(result)
